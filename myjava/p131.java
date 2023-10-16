c87c645f34a22024931fa53a083f8b321673fbc4
NaokiMizuta5 <naoki.mizuta.work@gmail.com>
2023-10-17 01:50:46 +0900

    Number 132 is finished. To be continued

---
+++ myjava/p131.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    static int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int current_pos = 0;
+        int moving_pos;
+        for (moving_pos = 1; moving_pos < nums.length; moving_pos++) {
+            if (nums[current_pos] != nums[moving_pos]) {
+                nums[current_pos + 1] = nums[moving_pos];
+                current_pos++;
+            }
+        }
+        return current_pos + 1;
+        // すなわち重複を除くと何個数字があるか返す
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,3,4,5,6,7,7};
+        System.out.println("Original array: " + Arrays.toString(nums));
+        System.out.println("The length of the original array is: "+nums.length);
+        System.out.println("After removing duplicates, the new length of the array is: "+ removeDuplicates(nums));
+    }
+}
